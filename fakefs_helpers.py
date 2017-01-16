d531dceda0f54f36c68c2daf723b5cb69cf9dc7e
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2017-01-16 11:47:26 +0100

    FakeFS: support unittest's ".addCleanup()" and provide a simplified ".set_up()" method

--- fakefs_helpers.py
+++ fakefs_helpers.py
@@ -1,17 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from pyfakefs.fake_filesystem_unittest import Patcher as PyFakeFSPatcher
 
 
-__all__ = ['FakeFS', 'PyFakeFSPatcher']
+__all__ = ['FakeFS']
 
 class FakeFS(object):
     def __init__(self, patcher):
         self._stubber = patcher
 
+    @classmethod
+    def set_up(cls, test=None):
+        # only require pyfakefs if it is actually used
+        from pyfakefs.fake_filesystem_unittest import Patcher
+        stubber = Patcher()
+        stubber.setUp()
+        if test:
+            test.addCleanup(stubber.tearDown)
+        return FakeFS(stubber)
+
+    def tear_down(self):
+        self._stubber.tearDown()
+
     def __getattr__(self, name):
         mapping = {
             'create_directory': self._stubber.fs.CreateDirectory,
+            'create_file': self._stubber.fs.CreateFile,
         }
         if name in mapping:
             return mapping[name]
