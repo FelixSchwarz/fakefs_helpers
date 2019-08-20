2fd30bee81374aa127c36a27387f78dc218025dc
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2019-08-20 13:26:43 +0200

    add project metadata

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import re
+
+from setuptools import setup, find_packages
+
+
+def requires_from_file(filename):
+    requirements = []
+    with open(filename, 'r') as requirements_fp:
+        for line in requirements_fp.readlines():
+            match = re.search('^\s*([a-zA-Z][^#]+?)(\s*#.+)?\n$', line)
+            if match:
+                requirements.append(match.group(1))
+    return requirements
+
+
+setup(
+    name='FakeFSHelpers',
+    version='1.0',
+
+    author='Felix Schwarz',
+    author_email='info@schwarz.eu',
+    license='MIT',
+
+    zip_safe=False,
+    packages=find_packages(),
+    namespace_packages = ['schwarz'],
+    include_package_data=True,
+
+    tests_require = requires_from_file('dev_requirements.txt'),
+    install_requires=requires_from_file('requirements.txt'),
+)
+
