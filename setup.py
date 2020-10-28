e4347b2ad67646431adf89f81dbe96804ea1154a
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2020-10-28 11:27:25 +0100

    move (testing) requirements to setup.cfg

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python
-import re
 
 from setuptools import setup
 
-
-def requires_from_file(filename):
-    requirements = []
-    with open(filename, 'r') as requirements_fp:
-        for line in requirements_fp.readlines():
-            match = re.search('^\s*([a-zA-Z][^#]+?)(\s*#.+)?\n$', line)
-            if match:
-                requirements.append(match.group(1))
-    return requirements
-
-
-setup(
-    tests_require = requires_from_file('dev_requirements.txt'),
-    install_requires=requires_from_file('requirements.txt'),
-)
+setup()
 
